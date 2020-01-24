2183782dae672426b0e55401ea9628259226d831
Colciago Gabriele <colciago.gabriele@osdbsesto>
2020-01-24 10:06:37 +0100

    add button and btn.eventListener

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var btn= document.createElement("button");
 btn.innerHTML="clicca";
 var body= document.getElementsByTagName("body")[0];
 body.append(btn);
-btn.addEventListener("click",function(){
+btn.addEventListener("click", function(){
     document.getElementById("h1").innerHTML="ciao come stai";
 });
